faf1947f70a94ebf120d7354f12ade1d58decd98
mohammad13790 <30660253+mohammad13790@users.noreply.github.com>
2017-08-03 13:58:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,7 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_username = 'ExampleHelperBot'  -- Input Helper Username Here Without @
+helper_username = 'ExampleHelperBot' 403343189 -- Input Helper Username Here Without @
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
@@ -90,7 +90,7 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {403343189,},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
